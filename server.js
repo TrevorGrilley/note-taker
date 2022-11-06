f84e7b73a15d79b93b4b06c48e8070409ec007e9
Trevor <trevorgrilley@yahoo.com>
2022-11-06 18:44:51 -0500

    add post and get routes

--- server.js
+++ server.js
@@ -4,3 +4,23 @@ const notes = require('./db/db.json');
 const app = express();
 const path = require('path');
 const fs = require('fs');
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+  })
+  
+  app.get('/notes', (req, res)=> {
+      res.sendFile(__dirname + '/public/notes.html');
+  })
+  
+  app.get('/api/notes', (req, res)=> {
+      res.json(notes);
+    })
+  
+    app.post('/api/notes', (req, res) => {
+        const newNote = createNewNote(req.body, allNotes);
+        res.json(newNote);
+    });
