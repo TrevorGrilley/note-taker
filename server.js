d8808df2bb69b1e904073ca6e7f1490425b88f52
Trevor <trevorgrilley@yahoo.com>
2022-11-06 20:19:22 -0500

    finsih get and post routes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
+//html routes start
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
   })
@@ -16,27 +17,37 @@ app.get('/', (req, res) => {
   app.get('/notes', (req, res)=> {
       res.sendFile(__dirname + '/public/notes.html');
   })
+  //html routes end
   
-  app.get('/api/notes', (req, res)=> {
-      res.json(data);
-    })
-  
+  //api routes start
+    app.get("/api/notes", function(req, res) {   
+    res.json(data);
+    });
+
+    function newNote(body, Array) {
+        const notes = body;
+        if (!Array.isArray(Array))
+            Array = [];
+        
+        if (Array.length === 0)
+            Array.push(0);
+    
+        body.id = Array[0];
+        Array[0]++;
+    
+        Array.push(notes);
+        fs.writeFileSync(
+            path.join(__dirname, './db/db.json'),
+            JSON.stringify(Array, null, 2)
+        );
+        return notes;
+    }
+    
     app.post('/api/notes', (req, res) => {
-      console.info(`${req.method} received request to create note`);
-  
-      const { title, text } = req.body;
-  
-      if(title && text){
-        const newNote ={
-          title,
-          text
-        }
-  
-        data.push(newNote)
-  
-      }
-      res.send(data);
-    })
+        const notes = newNote(req.body, allNotes);
+        res.json(notes);
+    });
+    
   
     app.listen(PORT, () => {
       console.log(`API server now on port ${PORT}!`);
